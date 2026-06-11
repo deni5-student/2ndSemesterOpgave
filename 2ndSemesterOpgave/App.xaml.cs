85f8203d0b1ea2ff703f0cafc5607c5a01e69c8c
deni5-student <deni5@student.eadania.dk>
2026-06-11 13:47:39 +0200

    Add project files.

---
+++ 2ndSemesterOpgave/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _2ndSemesterOpgave
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
